eb77334a15e5f833f13641c2ff1b652370ad2bdc
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-03-25 11:56:03 +0100

    test(config): debug ssh key

--- deployment/modules/nixosModules/ssh-root.nix
+++ deployment/modules/nixosModules/ssh-root.nix
@@ -7,6 +7,7 @@
 
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqVpl7w1HPMm5GPqTAMXKbBdPEiRZzMPqHwWI9EtoyT" # Bootstrap key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4BdXgpvrAV1m4S/djUnrNsdUDInRJcFzzriE9G+zyo" # FOR TESTING ONLY
     ];
   };
 }
