ee1da4f21843f57bf8c0803f49d738e228e0c122
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-03-25 11:44:54 +0100

    test(config): test setup with env vars

--- deployment/modules/nixosModules/common.nix
+++ deployment/modules/nixosModules/common.nix
@@ -7,5 +7,27 @@
       app-user
       ssh-root
     ];
+
+    #virtualisation.docker.enable = true;
+    virtualisation.oci-containers = {
+      backend = "podman";
+      containers.envtest = {
+        user = "999:999";
+        image = "mendhak/http-https-echo:40";
+        environmentFiles = [ "/run/agenix/.env" ];
+        ports = [ "80:8080" "443:8443" ];
+      };
+      #containers.test = {
+      #  image = "nginx";
+      #  ports = [ "80:80" ];
+      #};
+    };
+
+    systemd.services."podman-envtest" = {
+      after = [ "agenix.service" ];
+      partOf = [ "agenix.service" ]; # Restart on secret updates
+    };
+
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
   };
 }
